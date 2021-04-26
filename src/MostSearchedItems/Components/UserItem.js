226ab30a6f409f1f9dd00014f8cb9117a16e585f
Sahana <sahana200136@gmail.com>
2021-04-26 14:39:40 +0530

    latest update

---
+++ src/MostSearchedItems/Components/UserItem.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Avatar from '../../Shared/Components/UIElements/Avatar';
+import Card from '../../Shared/Components/UIElements/Card';
+import './UserItem.css';
+
+const UserItem = props => {
+    return (
+        <li className="user-item">
+          <Card className="user-item__content">
+            <Link to="/about">
+              <div className="user-item__image">
+                <Avatar image={props.image} alt={props.name} />
+              </div>
+              <div className="user-item__info">
+                <h2>{props.name}</h2>
+                <h3>
+                  File Here
+                  <br />
+                  Branch: {props.dept}
+                </h3>
+              </div>
+            </Link>
+          </Card>
+        </li>
+      );
+};
+
+export default UserItem;
